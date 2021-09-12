f5dd38e8dbca43d1d7250f3ce1e8125ab5d1e3fd
AYehia0 <ahmedyehia611@gmail.com>
2021-09-12 22:26:23 +0200

    added route to handle twitter api things

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const configs = require('./config')
 // node 
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 // creating the app
 const app = express()
@@ -13,15 +14,26 @@ const PORT = configs.server_configs.port || 6969
 
 // middlewares
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 
 // routes
 // home
 app.get('/', (req, res, err) => {
-    
     res.sendFile('index.html')
-
 })
 
+// getting the link
+app.post('/send-link', (req, res, err) => {
+    const tweetUrl = req.body.link
+
+    // getting tweet's info
+    twitter.getTweetInfo(tweetUrl, (data) => {
+
+        // reply back
+        // sending back the data
+        res.send(data)
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
