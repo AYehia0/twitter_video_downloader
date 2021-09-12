7e35aeb2a674e163f1b5b0629f0d848c32d4f83f
AYehia0 <ahmedyehia611@gmail.com>
2021-09-12 22:25:53 +0200

    getting url and making post request

---
+++ public/script.js
@@ -0,0 +1,64 @@
+const form = document.getElementById('submit-form')
+const linkInput = document.getElementById('get-link')
+const resultContatiner = document.querySelector('.result')
+
+const getDataFromApi = async (apiRoute, link) => {
+
+    const res = await fetch(apiRoute, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({link})
+    })
+
+    const data = await res.json()
+
+    return data
+}
+
+// displaying the tweet's info
+const showResult = (data) => {
+
+    const htmlTemp = `
+        <div class="result">
+            <img src="${data.video_img}" alt="">
+
+            <video controls class="vid">
+                <source src="${data.download_url}" type="video/mp4">
+            </video> 
+
+            <span class="duration">Duration: ${data.duration}</span>
+
+            <a href="${data.download_url}" target="_blank" download><button class="download-btn">Download Vid</button></a>
+
+        </div>
+    `
+    resultContatiner.innerHTML = htmlTemp
+
+}
+
+form.addEventListener('submit', async (e) => {
+
+    // preventing the page from reloading
+    e.preventDefault()
+
+    // getting the link
+    const link = linkInput.value
+
+    if (link){
+        const data = await getDataFromApi('/send-link', link)
+
+        console.log(data)
+
+        // showing the data to the html
+        showResult(data)
+
+        // resetting 
+        linkInput.value = ""
+
+
+    }
+
+
+})
